a10a462cf838584e1ca340126fc1f35d33182d70
Sumit Kumar <j4sumit@gmail.com>
2024-03-09 23:57:57 +0530

    routing practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p id="zero-state">
+      This is a demo for React Router.
+      <br />
+      Check out{" "}
+      <a href="https://reactrouter.com">
+        the docs at reactrouter.com
+      </a>
+      .
+    </p>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,93 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {createRoutesFromElements,createBrowserRouter,Route,RouterProvider} from "react-router-dom";
+
+import "./index.css"
+import Root,{ loader as rootLoader,action as rootAction, } from './routes/root';
+import ErrorPage from './ErrorPage';
+import Contact, {loader as contactLoader,   action as contactAction,} from './routes/contact';
+import EditContact,{action as editAction} from './routes/edit';
+import {action as destroyAction } from './routes/destroy'
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root/>,
+//     errorElement: <ErrorPage/>,
+//     loader: rootLoader,
+//     action: rootAction,
+   
+//   children:[
+//    {
+//     errorElement: <ErrorPage />,
+//     children:[
+//       { index: true, element: <App /> },
+//     {
+//       path: "contacts/:contactId",
+//       element: <Contact/>,
+//       loader: contactLoader,
+//       action: contactAction,
+
+//     },
+//     {
+//       path: "contacts/:contactId/edit",
+//       element: <EditContact/>,
+//       loader: contactLoader,
+//       action: editAction,
+//     },
+//     {
+//       path: "contacts/:contactId/destroy",
+//       action: destroyAction,
+//       errorElement: <div>Oops! There was an error.</div>,
+//     },
+//   ],
+
+//    } 
+//   ]
+// },
+
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<App />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path="contacts/:contactId/destroy"
+          action={destroyAction}
+          
+        />
+      </Route>
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
